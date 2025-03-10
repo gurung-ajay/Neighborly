d319b7bf5756b18643e645d6e441b3567b11d6a9
Ajay Gurung <gooronz.ajay@gmail.com>
2025-03-10 20:53:17 +0545

    login feature added

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -18,4 +18,17 @@ const createUser = async (req, res) => {
     }
 }
 
-export { getUser, createUser }
+
+const Login = async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email })
+        if (!user) return res.status(404).json({ message: 'User not found' })
+        const passwordMatch = await bcrypt.compare(req.body.password, user.password)
+        if (!passwordMatch) return res.status(401).json({ message: 'Invalid password' })
+        res.status(200).json({ message: 'Login successful' })
+    } catch (error) {
+        res.status(500).json({ message: 'Failed to login' })
+    }
+}
+
+export { getUser, createUser, Login }
--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -1,9 +1,10 @@
 import Router from 'express'
-import { getUser, createUser } from '../controllers/user.controller.js'
+import { getUser, createUser, Login } from '../controllers/user.controller.js'
 
 const routes = Router()
 
 routes.get('/', getUser)
 routes.post('/register', createUser)
+routes.post('/login', Login)
 
 export default routes
